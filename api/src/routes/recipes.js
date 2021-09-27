8bfdcfef42ce0180864b878a89d57509eb3caed7
Miguegithub78 <avallemiguel@hotmail.com>
2021-09-26 23:40:46 -0300

    modifico recipes

--- api/src/routes/recipes.js
+++ api/src/routes/recipes.js
@@ -3,13 +3,13 @@ const router = express.Router()
 const axios = require('axios');
 require('dotenv').config();
 const { Recipe, Type, Op } = require('../db');
-const {API_KEY, spoonacularURL} = process.env;
+const {API_KEY, spoonacularURL} = api.env;
 
 router.get('/', async (req, res) => {
         const { name } = req.query
 
         if(!name){
-            const resAxios = await axios.get(`https://api.spoonacular.com/recipes/complexSearch?apiKey=d524b9a5a36e4ef6bd9fac6808e213c9&addRecipeInformation=true&number=100`);
+            const resAxios = await axios.get(`${spoonacularURL}/recipes/complexSearch?apiKey=${API_KEY}&addRecipeInformation=true&number=100`);
             const { number, totalResults, results} = resAxios.data ;
             let response = []
             if (results.length > 0) {
@@ -21,7 +21,7 @@ router.get('/', async (req, res) => {
             }
             res.json(response);
         }else{
-            // https://api.spoonacular.com/recipes/complexSearch?query=Chicken%20and%20Mango%20Skewer&addRecipeInformation=true&number=100&apiKey=d524b9a5a36e4ef6bd9fac6808e213c9
+            // https://api.spoonacular.com/recipes/complexSearch?query=Chicken%20and%20Mango%20Skewer&addRecipeInformation=true&number=100&apiKey=
         }
 })
 
