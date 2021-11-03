fb31490778964047b81a0d2f503b343ef0c708f6
Miguegithub78 <avallemiguel@hotmail.com>
2021-11-03 09:55:56 -0300

    process.env

--- api/index.js
+++ api/index.js
@@ -1,11 +1,11 @@
 require("dotenv").config();
 const server = require('./src/app.js');
 const { conn, Diet } = require('./src/db.js');
-const { PORT } = process.env;
+
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  server.listen(PORT, () => {
+  server.listen(process.env.PORT, () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
     const dietas = ["gluten free","paleolithic", "vegetarian", "lacto ovo vegetarian","vegan","pescatarian","paleo","primal","whole 30", "fodmap friendly", "fruitarian", "dairyFree"];
     dietas.forEach(async (element) => await Diet.create({name: element}));
